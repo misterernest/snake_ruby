7438b13649ceae8779bac02520572678ce78aa58
Ernesto Campos Villa <misterernest@hotmail.com>
2019-01-23 09:18:36 -0500

    add manager of game

---
+++ src/app.rb
@@ -0,0 +1,19 @@
+require_relative 'view/Ruby2D'
+require_relative 'model/state'
+
+
+
+class App
+  def start
+    view = View::Ruby2dView.new
+    initial_state = Model.initial_state
+    view.render(initial_state)
+  end
+
+  def init_timer
+    loop do
+      sleep 0.5
+      #trigger movement
+    end
+  end
+end
