936d0b2e5aa8344e2ccfd31a8c9ead82e46cb116
Ernesto Campos Villa <holasoy@misterernest.com>
2019-01-30 23:13:19 -0500

    add actions feature  over comportment of the snake and yours food

--- src/actions/actions.rb
+++ src/actions/actions.rb
@@ -1,47 +1,87 @@
 module Actions
   def self.move_snake(state)
-    next_direction = state.next_direction
+    # next_direction = state.current_direction
     next_position = calc_next_position(state)
     # verficar si la siguiente casilla es valida para terminar o continuar el juego
-    if position_is_valid?(state, next_position)
+    if position_is_food?(state, next_position)
+      grow_snake_to(state, next_position)
+      generate_food state
+    elsif position_is_valid?(state, next_position)
       move_snake_to(state, next_position)
     else
       end_game(state)
     end
   end
 
-  private
-
-  def calc_next_position(state)
+  def self.calc_next_position(state)
     current_position = state.snake.positions.first
-    case state.next_direction
-    when UP
-      Model::Cord.new(current_position.row - 1, current_position.col)
-    when DOWN
-      Model::Cord.new(current_position.row + 1, current_position.col)
-    when RIGHT
-      Model::Cord.new(current_position.row, current_position.col + 1)
-    when LEFT
-      Model::Cord.new(current_position.row, current_position.col - 1)
+    case state.current_direction
+    when Model::Direction::UP
+      Model::Coord.new(current_position.row - 1, current_position.col)
+    when Model::Direction::DOWN
+      Model::Coord.new(current_position.row + 1, current_position.col)
+    when Model::Direction::RIGHT
+      Model::Coord.new(current_position.row, current_position.col + 1)
+    when Model::Direction::LEFT
+      Model::Coord.new(current_position.row, current_position.col - 1)
     end
   end
 
-  def position_is_valid?(state, position)
-    is_invalid =  postion.row >= state.grid.rows || position.row <= 0 ||
-                  postion.col >= state.grid.cols || position.cols <= 0
+  def self.grow_snake_to(state, next_position)
+    new_positions = [next_position] + state.snake.positions
+    state.snake.positions = new_positions
+    state
+  end
+
+  def self.position_is_food?(state, next_position)
+    state.food.row == next_position.row && state.food.col == next_position.col
+  end
+  def self.position_is_valid?(state, position)
+    is_invalid =  position.row >= state.grid.rows || position.row < 0 ||
+                  position.col >= state.grid.cols || position.col < 0
 
     return false if is_invalid
 
-    !(state.snake.positions.include? postion)
+    !(state.snake.positions.include? position)
   end
 
-  def move_snake_to(state, next_position)
+  def self.move_snake_to(state, next_position)
     new_positions = [next_position] + state.snake.positions[0...-1]
     state.snake.positions = new_positions
     state
   end
 
-  def end_game(state)
+  def self.end_game(state)
     state.game_finished = true
+    state
+  end
+
+  def self.change_direction(state, direction)
+    if next_direction_is_valid? state, direction
+      state.current_direction = direction
+    else
+      puts 'Invalid direction'
+    end
+    state
+  end
+
+  def self.next_direction_is_valid?(state, direction)
+    case state.current_direction
+    when Model::Direction::UP
+      return true if direction != Model::Direction::DOWN
+    when Model::Direction::DOWN
+      return true if direction != Model::Direction::UP
+    when Model::Direction::RIGHT
+      return true if direction != Model::Direction::LEFT
+    when Model::Direction::LEFT
+      return true if direction != Model::Direction::RIGHT
+    end
+    false
+  end
+
+  def self.generate_food(state)
+    new_food = Model::Food.new rand(state.grid.rows), rand(state.grid.cols)
+    state.food = new_food
+    state
   end
 end
