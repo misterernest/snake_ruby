c47970ada45f0895d9fb58e4d06171a6f859f148
Ernesto Campos Villa <holasoy@misterernest.com>
2019-01-30 23:17:00 -0500

    add manager of render of game

--- src/view/ruby2d.rb
+++ src/view/ruby2d.rb
@@ -1,30 +1,43 @@
 require 'Ruby2D'
+require_relative '../model/state'
 
 module View
+  # TODO: INTRODUCIR COMENTARIOS
   class Ruby2dView
 
-    def initialize
+    def initialize(app)
       @pixel_size = 50
+      @app = app
     end
 
-    def render(state)
+    def start(state)
       extend Ruby2D::DSL
       set(
         title: 'Snake',
         width: @pixel_size * state.grid.cols,
         height: @pixel_size * state.grid.rows
       )
+      on :key_down do |event|
+        # A key was pressed
+        handle_key_event event
+      end
+      show
+    end
+
+    def render(state)
+      extend Ruby2D::DSL
+      close if state.game_finished
       render_foot(state)
       render_snake(state)
-      show
     end
 
     private
 
     def render_foot(state)
+      @food.remove if @food
       extend Ruby2D::DSL
       food = state.food
-      Square.new(
+      @food = Square.new(
         x: food.col * @pixel_size,
         y: food.row * @pixel_size,
         size: @pixel_size,
@@ -33,15 +46,28 @@ module View
     end
 
     def render_snake(state)
+      @snake_positions.each(&:remove) if @snake_positions
       extend Ruby2D::DSL
       snake = state.snake
-      snake.positions.each do |pos|
+      @snake_positions = snake.positions.map do |pos|
         Square.new(
-          x: pos.col * @pixel_size,
-          y: pos.row * @pixel_size,
-          size: @pixel_size,
-          color: 'green'
+          x: pos.col * @pixel_size, y: pos.row * @pixel_size,
+          size: @pixel_size, color: 'green'
         )
+      end
+    end
+    private
+
+    def handle_key_event(event)
+      case event.key
+      when 'up'
+        @app.send_action :change_direction, Model::Direction::UP
+      when 'down'
+        @app.send_action :change_direction, Model::Direction::DOWN
+      when 'left'
+        @app.send_action :change_direction, Model::Direction::LEFT
+      when 'right'
+        @app.send_action :change_direction, Model::Direction::RIGHT
       end
     end
   end
